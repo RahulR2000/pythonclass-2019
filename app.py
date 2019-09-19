da2ce3c39fc6c28b4ee508f3a64f72c1fe1ab2f5
RahulR2000 <rahulrajesh4950@gmail.com>
2019-09-19 14:23:34 +0530

    update

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask,render_template
 
 
 app = Flask(__name__)
-
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route("/home")
 def home():
-    return "My home page"
+    return render_template('home.html')
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
